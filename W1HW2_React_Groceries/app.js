92b8662d0d82f1a3f6eade97c76ebb5c8b622039
Vivian Ng <vivian.ng.t.y@gmail.com>
2021-01-03 16:02:12 -0500

    second attempt

--- W1HW2_React_Groceries/app.js
+++ W1HW2_React_Groceries/app.js
@@ -1,7 +1,63 @@
+/* console.log (items); */
+
 class App extends React.Component {
+    state = {
+        items: items,
+        name: '',
+        brand: '',
+        units: '',
+        quantity: '',
+        isPurhased: false
+    }
+
+    handleChange = (e) => {
+        this.setState ({ [e.target.id] : e.target.value})
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault ();
+        const newItem = {
+            name: this.state.name,
+            brand: this.state.brand,
+            units: this.state.units,
+            quantity: this.state.quantity
+        }
+        this.setState ({
+            items: [ newItem, ...this.state.items],
+            name: '',
+            brand: '',
+            units: '',
+            quantity: ''
+        })
+
+    }
+
     render () {
         return (
-
+            <div>
+                <h1>Grocery List</h1>
+                <ul>
+                    {this.state.items.map (item => {
+                        return (
+                            <li>{item.name } {item.brand } {item.units } Quantity: {item.quantity }</li>)
+                    })}
+                </ul>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">Name:  </label>
+                    <input type="text" value={this.state.name} onChange={this.handleChange} id='name' placeholder='name of item' />
+                    <br />
+                    <label htmlFor="brand">Brand:  </label>
+                    <input type="text" value={this.state.brand} onChange={this.handleChange} id="brand" placeholder='brand of item' />
+                    <br />
+                    <label htmlFor="units">Unit:  </label>
+                    <input type="text" value={this.state.units} onChange={this.handleChange} id="units" placeholder='units of item' />
+                    <br/>
+                    <label htmlFor="quantity">Quantity:  </label>
+                    <input type="text" value={this.state.quantity} onChange={this.handleChange} id="quantity" placeholder='quantity of item' />
+                    <br />
+                    <input type="submit" />
+                </form>
+            </div>
         )
     }
 }
---
+++ W1HW2_React_Groceries/data.js
@@ -0,0 +1,32 @@
+const items = [
+    {
+        'name': 'Gummy Bear',
+        'brand': 'Haribo',
+        'unit': '1 lb',
+        'quantity': '1'
+    },
+    {
+        'name': 'Water',
+        'brand': 'Fiji',
+        'unit': '24 L',
+        'quantity': '3'
+    },
+    {
+        'name': 'Extra Virgin Olive Oil',
+        'brand': 'California Oilve Ranch',
+        'unit': '550 mL',
+        'quantity': '1'
+    },
+    {
+        'name': 'Almond milk',
+        'brand': 'Blue Diamond Almond Breeze',
+        'unit': '0.5 gal',
+        'quantity': '2'
+    },
+    {
+        'name': 'Soda',
+        'brand': 'Pepsi',
+        'unit': '12 oz',
+        'quantity': '4'
+    },
+]
